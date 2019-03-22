012895a28ad6a7039899f27268760e96d73af3e2
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-03-22 09:48:40 -0400

    Setup version 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='face_detector',
-     version='0.1',
+     version='0.2',
      scripts=['scripts/zlib_install'] ,
      author="Luis Rojas Aguilera",
      author_email="rojas@icomp.ufam.edu.br",
