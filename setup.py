a48d3f77cd8a861639f7799d359dd5fbb5e3a62f
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-03-22 17:04:13 -0400

    Added dependencies

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt', 'rt') as f:
+    dependencies = f.read().split('\n')
+
 setuptools.setup(
      name='face_detector',
-     version='0.2',
+     version='0.2.1',
      scripts=['scripts/zlib_install'] ,
      author="Luis Rojas Aguilera",
      author_email="rojas@icomp.ufam.edu.br",
@@ -14,6 +17,7 @@ setuptools.setup(
      long_description_content_type="text/markdown",
      url="https://github.com/roj4s/face_detector",
      packages=setuptools.find_packages(),
+     install_requires=dependencies,
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
